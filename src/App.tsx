fe550b2b8100dd1ff07eb690e21c1ab020044bb2
Narek Aleksanyan <narek.alexanyan.96@gmail.com>
2023-03-22 16:54:42 +0400

    Created Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,20 @@ import { Container } from "@chakra-ui/react";
 import Home from "./pages/Home";
 import Store from "./pages/Store";
 import About from "./pages/About";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <Container maxW="8xl">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/store" element={<Store />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </Container>
+    <>
+    <Navbar />
+      <Container maxW="8xl">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </>
   );
 };
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,124 @@
+import { ReactNode } from "react";
+import {
+  Box,
+  Flex,
+  HStack,
+  Link,
+  IconButton,
+  Button,
+  useDisclosure,
+  useColorModeValue,
+  Container,
+} from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+import { NavLink as RLink } from "react-router-dom";
+
+interface linkData {
+  id: number | string;
+  title: string;
+  path: string;
+}
+
+interface navLink {
+  children: ReactNode;
+  path: string;
+}
+
+const Links: Array<linkData> = [
+  {
+    id: 1,
+    title: "Home",
+    path: "/",
+  },
+  {
+    id: 2,
+    title: "Store",
+    path: "/store",
+  },
+  {
+    id: 3,
+    title: "About",
+    path: "/about",
+  },
+];
+
+const NavLink = ({ children, path }: navLink) => (
+  <Link
+    as={RLink}
+    to={path}
+    px={2}
+    py={1}
+    rounded={"md"}
+    _hover={{
+      textDecoration: "none",
+      bg: useColorModeValue("gray.200", "gray.700"),
+    }}
+  >
+    {children}
+  </Link>
+);
+
+export default function Simple() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+        <Container maxW="8xl">
+          <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+            <IconButton
+              size={"md"}
+              icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+              aria-label={"Open Menu"}
+              display={{ md: "none" }}
+              onClick={isOpen ? onClose : onOpen}
+            />
+            <HStack spacing={8} alignItems={"center"}>
+              <Box>Logo</Box>
+              <HStack
+                as={"nav"}
+                spacing={4}
+                display={{ base: "none", md: "flex" }}
+              >
+                {Links.map((link) => (
+                  <NavLink key={link.id} path={link.path}>
+                    {link.title}
+                  </NavLink>
+                ))}
+              </HStack>
+            </HStack>
+            <Button pos="relative">
+              <svg
+                width="20"
+                height="20"
+                viewBox="0 0 20 20"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M1 1H3L3.4 3M5 11H15L19 3H3.4M5 11L3.4 3M5 11L2.70711 13.2929C2.07714 13.9229 2.52331 15 3.41421 15H15M15 15C13.8954 15 13 15.8954 13 17C13 18.1046 13.8954 19 15 19C16.1046 19 17 18.1046 17 17C17 15.8954 16.1046 15 15 15ZM7 17C7 18.1046 6.10457 19 5 19C3.89543 19 3 18.1046 3 17C3 15.8954 3.89543 15 5 15C6.10457 15 7 15.8954 7 17Z"
+                  stroke="#111827"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+              <Box
+                bg="tomato"
+                w="20px"
+                h="20px"
+                pos="absolute"
+                top="-5px"
+                right="0"
+                zIndex={2}
+                borderRadius="full"
+                color="white"
+                fontSize="md"
+              >1</Box>
+            </Button>
+          </Flex>
+        </Container>
+      </Box>
+    </>
+  );
+}
