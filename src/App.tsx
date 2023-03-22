875be31f07d41638613d99624022960727691a53
Narek Aleksanyan <narek.alexanyan.96@gmail.com>
2023-03-22 15:58:12 +0400

    Added chakra ui and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
-function App() {
-  return <div className="App"></div>;
-}
+import { Routes, Route } from "react-router-dom";
+import { Container } from "@chakra-ui/react";
+import Home from "./pages/Home";
+import Store from "./pages/Store";
+import About from "./pages/About";
+
+const App = () => {
+  return (
+    <Container maxW="8xl">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Container>
+  );
+};
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+};
+
+const theme = extendTheme({ colors });
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
+  </React.StrictMode>
+);
